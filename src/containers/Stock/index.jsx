569a2bf41aa59e645b4ced418855a56ea4d696ee
harry-chiu <harrychiu1106@gmail.com>
2025-01-05 17:56:17 +0800

    feat: dynamic column

--- src/containers/Stock/index.jsx
+++ src/containers/Stock/index.jsx
@@ -3,66 +3,87 @@ import { useFormik } from 'formik';
 import { Table, Space, Button, InputNumber } from 'antd';
 import { useWindowSize } from 'react-use';
 import { toast } from 'react-toastify';
-import { Container, FixedBottomBlock } from './styled';
+import {
+  Container,
+  FixedTopBlock,
+  FixedBottomBlock,
+  TableBlock,
+} from './styled';
+
+const getStockId = index => `stock-${index + 1}`;
+
+const getStockTitle = index => `No.${index + 1}`;
+
+const getQuantityId = index => `quantity-${index + 1}`;
 
 const FIELD_NAME = {
   STOCKS: 'stocks',
+  COLUMN_COUNT: 'columnCount',
 };
 
 const INITIAL_VALUES = {
-  stocks: Array(50)
+  stocks: Array(30)
     .fill(0)
     .map((_, index) => ({
-      id: index,
-      title: `No.${index + 1}`,
-      quantity1: 0,
-      quantity2: 0,
+      id: getStockId(index),
+      title: getStockTitle(index),
     })),
+  columnCount: 1,
 };
-
 const Stock = () => {
   const { height: windowHeight } = useWindowSize();
   const formik = useFormik({ initialValues: INITIAL_VALUES });
 
-  const handleQuantity1Add = record => () => {
+  const getStockIndex = stockId =>
+    formik.values.stocks.findIndex(stock => stock.id === stockId);
+
+  const handleQuantityAdd = (record, index) => () => {
+    const stockIndex = getStockIndex(record.id);
+    const quantityId = getQuantityId(index);
+    const value = record[quantityId] || 0;
+    const newValue = value + 1;
     formik.setFieldValue(
-      `${FIELD_NAME.STOCKS}.${record.id}.quantity1`,
-      record.quantity1 + 1,
+      `${FIELD_NAME.STOCKS}.${stockIndex}.${quantityId}`,
+      newValue,
     );
   };
 
-  const handleQuantity1Minus = record => () => {
-    const newValue = record.quantity1 - 1;
+  const handleQuantityMinus = (record, index) => () => {
+    const stockIndex = getStockIndex(record.id);
+    const quantityId = getQuantityId(index);
+    const value = record[quantityId] || 0;
+    const newValue = value - 1;
     formik.setFieldValue(
-      `${FIELD_NAME.STOCKS}.${record.id}.quantity1`,
+      `${FIELD_NAME.STOCKS}.${stockIndex}.${quantityId}`,
       newValue > 0 ? newValue : 0,
     );
   };
 
-  const handleQuantity2Add = record => () => {
+  const handleQuantityChange = (record, index) => newValue => {
+    const stockIndex = getStockIndex(record.id);
+    const quantityId = getQuantityId(index);
     formik.setFieldValue(
-      `${FIELD_NAME.STOCKS}.${record.id}.quantity2`,
-      record.quantity2 + 1,
-    );
-  };
-
-  const handleQuantity2Minus = record => () => {
-    const newValue = record.quantity2 - 1;
-    formik.setFieldValue(
-      `${FIELD_NAME.STOCKS}.${record.id}.quantity2`,
-      newValue > 0 ? newValue : 0,
+      `${FIELD_NAME.STOCKS}.${stockIndex}.${quantityId}`,
+      newValue,
     );
   };
 
   const handleCopyReportText = () => {
-    const stocks = formik.values.stocks?.filter(
-      stock => stock.quantity1 || stock.quantity2,
+    const stocks = formik.values.stocks?.filter(stock =>
+      Array(formik.values.columnCount)
+        .fill(0)
+        .some((_, index) => stock[getQuantityId(index)] > 0),
     );
-    const reportText = stocks.reduce(
-      (text, stock) =>
-        (text += `${stock.title}: ${stock.quantity1} + ${stock.quantity2}\n`),
-      '',
-    );
+    const reportText = stocks.reduce((text, stock) => {
+      return (
+        (text +=
+          `${stock.title}: ` +
+          Array(formik.values.columnCount)
+            .fill(0)
+            .map((_, index) => `${stock[getQuantityId(index)] || 0}`)
+            .join(' + ')) + '\n'
+      );
+    }, '');
     navigator.clipboard.writeText(reportText).then(() => {
       toast.success('報表已複製到剪貼簿');
     });
@@ -79,38 +100,81 @@ const Stock = () => {
       title: '商品',
       width: 80,
     },
-    {
-      key: 'stock-1',
-      title: '庫存 1',
-      render: record => (
-        <Space>
-          <Button onClick={handleQuantity1Minus(record)}>-</Button>
-          <InputNumber min={0} value={record.quantity1} />
-          <Button onClick={handleQuantity1Add(record)}>+</Button>
-        </Space>
-      ),
-    },
-    {
-      key: 'stock-2',
-      title: '庫存 2',
-      render: record => (
-        <Space>
-          <Button onClick={handleQuantity2Minus(record)}>-</Button>
-          <InputNumber min={0} value={record.quantity2} />
-          <Button onClick={handleQuantity2Add(record)}>+</Button>
-        </Space>
-      ),
-    },
+    ...Array(formik.values.columnCount)
+      .fill(0)
+      .map((_, index) => ({
+        key: getStockId(index),
+        title: `庫存 ${index + 1}`,
+        render: record => (
+          <Space>
+            <Button onClick={handleQuantityMinus(record, index)}>-</Button>
+            <InputNumber
+              min={0}
+              value={record[getQuantityId(index)] || 0}
+              onChange={handleQuantityChange(record, index)}
+            />
+            <Button onClick={handleQuantityAdd(record, index)}>+</Button>
+          </Space>
+        ),
+      })),
   ];
+
+  React.useEffect(() => {
+    formik.setFieldValue(
+      FIELD_NAME.STOCKS,
+      formik.values.stocks.map(stock => {
+        const newStock = { ...stock };
+        Array(formik.values.quantityCount)
+          .fill(0)
+          .forEach((_, index) => {
+            const quantityId = getQuantityId(index);
+            newStock[quantityId] = newStock[quantityId] || 0;
+          });
+        return newStock;
+      }),
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [formik.values.quantityCount]);
 
   return (
     <Container>
-      <Table
-        columns={columns}
-        dataSource={formik.values.stocks}
-        pagination={false}
-        scroll={{ y: windowHeight - 64 - 55 }}
-      />
+      <FixedTopBlock>
+        <Space>
+          <Button
+            onClick={() => {
+              formik.setFieldValue(
+                FIELD_NAME.COLUMN_COUNT,
+                formik.values.columnCount + 1,
+              );
+            }}
+          >
+            新增庫存
+          </Button>
+
+          <Button
+            danger
+            onClick={() => {
+              if (formik.values.columnCount > 1) {
+                formik.setFieldValue(
+                  FIELD_NAME.COLUMN_COUNT,
+                  formik.values.columnCount - 1,
+                );
+              }
+            }}
+          >
+            減少庫存
+          </Button>
+        </Space>
+      </FixedTopBlock>
+
+      <TableBlock>
+        <Table
+          columns={columns}
+          dataSource={formik.values.stocks}
+          pagination={false}
+          scroll={{ y: windowHeight - 64 - 55 }}
+        />
+      </TableBlock>
 
       <FixedBottomBlock>
         <Space>
--- src/containers/Stock/styled.js
+++ src/containers/Stock/styled.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  max-height: 100%;
+  overflow: hidden;
+
   .ant-input-number {
     width: 52px !important;
   }
@@ -8,6 +11,19 @@ export const Container = styled.div`
   .ant-btn-default {
     padding: 4px 12px;
   }
+`;
+
+export const FixedTopBlock = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  padding: 0 16px;
+  width: 100%;
+  height: 64px;
+  background: #ffffff;
+  border-bottom: 1px solid #f0f0f0;
 `;
 
 export const FixedBottomBlock = styled.div`
@@ -21,4 +37,9 @@ export const FixedBottomBlock = styled.div`
   height: 64px;
   background: #ffffff;
   border-top: 1px solid #f0f0f0;
+`;
+
+export const TableBlock = styled.div`
+  padding-top: 64px;
+  height: calc(100% - 64px);
 `;
